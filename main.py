cee44bac28b82772eb460cb9bfbe184fc2cbc857
Hetav01 <hp9902@g.rit.edu>
2025-01-19 13:45:48 -0500

    tried using yt_dlp instead of pytube but doesn't work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
-from pytube import YouTube
+# from pytube import YouTube
+# import yt_dlp
 import os
 import shutil
 from dotenv import load_dotenv
@@ -10,36 +11,55 @@ from zipfile import ZipFile
 load_dotenv()
 
 # Set the OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
-@st.cache
+@st.cache_data
 
 def load_model():
     model = whisper.load_model("base")
     return model
     
 
+# def save_audio(url):
+#     yt = YouTube(url)
+#     video = yt.streams.filter(only_audio=True).first()
+    
+#     out_file = video.download()     # Download the file
+#     root, ext = os.path.splitext(out_file)
+    
+#     file_name = root + ".mp3"
+    
+#     try: 
+#         os.rename(out_file, file_name)  # Rename the file
+#     except OSError as e:
+#         os.remove(file_name)
+#         os.rename(out_file, file_name)
+    
+#     audio_filename = Path(file_name).stem + ".mp3"
+    
+#     print(f"{yt.title} has been successfully downloaded")     # Success message
+#     print(file_name)
+    
+#     return yt.title, audio_filename
+
 def save_audio(url):
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        file_name = ydl.prepare_filename(info_dict)
+        root, ext = os.path.splitext(file_name)
+        audio_filename = root + ".mp3"
     
-    out_file = video.download()     # Download the file
-    root, ext = os.path.splitext(out_file)
-    
-    file_name = root + ".mp3"
-    
-    try: 
-        os.rename(out_file, file_name)  # Rename the file
-    except OSError as e:
-        os.remove(file_name)
-        os.rename(out_file, file_name)
-    
-    audio_filename = Path(file_name).stem + ".mp3"
-    
-    print(f"{yt.title} has been successfully downloaded")     # Success message
-    print(file_name)
-    
-    return yt.title, audio_filename
+    return info_dict['title'], audio_filename
 
 
 def save_audio_to_transcript(audio_file):
@@ -51,4 +71,14 @@ def save_audio_to_transcript(audio_file):
     return transcript
 
 #write the streamlit code (frontend for prototyping)
+
+st.markdown("# **YouTube video to Transcript Converter**")
+
+st.header("**Enter the YouTube video URL**")
+url_link = st.text_input("Enter the video URL here: ")
+
+if st.checkbox("Start Analysis"):
+    video_title, audio_filename = save_audio(url_link)
+    st.audio(audio_filename)
+
 
