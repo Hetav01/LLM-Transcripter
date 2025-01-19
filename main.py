f3c330b53f137245b5dc0fa86d2709dfc23b1c83
Hetav01 <hp9902@g.rit.edu>
2025-01-19 13:23:50 -0500

    built the initial setup

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ load_dotenv()
 # Set the OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+@st.cache
+
 def load_model():
     model = whisper.load_model("base")
     return model
@@ -26,3 +28,27 @@ def save_audio(url):
     
     file_name = root + ".mp3"
     
+    try: 
+        os.rename(out_file, file_name)  # Rename the file
+    except OSError as e:
+        os.remove(file_name)
+        os.rename(out_file, file_name)
+    
+    audio_filename = Path(file_name).stem + ".mp3"
+    
+    print(f"{yt.title} has been successfully downloaded")     # Success message
+    print(file_name)
+    
+    return yt.title, audio_filename
+
+
+def save_audio_to_transcript(audio_file):
+    model = load_model()
+    
+    result = model.transcribe(audio_file)
+    transcript = result["text"]
+    
+    return transcript
+
+#write the streamlit code (frontend for prototyping)
+
